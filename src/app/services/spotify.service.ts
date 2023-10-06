6475681b5f17cf6e3cedfeab83e3d0312b675d3e
Aura <aurasofiapg@gmail.com>
2023-10-05 19:23:43 -0500

    first commit

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor(private http: HttpClient) {
+    console.log('Spotify Service Listo');
+  }
+
+  getQuery( query: string ) {
+
+    const url = `https://api.spotify.com/v1/${ query }`;
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQD6v7enwRlA-R7nFgl7temOq3pwRv3SdZ95Q6Q8w5sDWgjnGv6StQcF8VU6kCOaozrm9nYMVTI2KAlXOEX57pOoqNXA86fu2FCIuABnsw51aVn3V8c'
+    });
+
+    return this.http.get(url, { headers });
+
+  }
+
+
+  getAlbumTheHives(){
+    return this.getQuery('albums/6agQKhrctciHD4QH7KufOS')
+            .pipe( map( data => data ));
+  }
+
+  getAlbumDojaCat(){
+    return this.getQuery('albums/6DmPNcfpkXBVRJsEIJY9tl')
+            .pipe( map( data => data ));
+  }
+
+  getAlbumAlbum5(){
+    return this.getQuery('albums/6DmPNcfpkXBVRJsEIJY9tl')
+            .pipe( map( data => data ));
+  }
+
+  getNewReleases() {
+    return this.getQuery('browse/new-releases?limit=20')
+              .pipe( map( data => data['albums'].items ));
+  }
+
+  getArtistas( termino: string ) {
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
+                .pipe( map( data => data['artists'].items));
+  }
+
+  getArtista( id: string ) {
+    return this.getQuery(`artists/${ id }`);
+                // .pipe( map( data => data['artists'].items));
+  }
+
+  getTopTracks( id: string ) {
+    return this.getQuery(`artists/${ id }/top-tracks?country=us`)
+                .pipe( map( data => data['tracks']));
+  }
+
+}
